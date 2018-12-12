b514742a2e8390e988b43eefe9e28a0e2f8a8543
KokoTa <584847514@qq.com>
2018-12-12 22:44:11 +0800

    update: particle.js

--- 2018/src/particle.js
+++ 2018/src/particle.js
@@ -2,33 +2,72 @@
  * @Author: KokoTa
  * @Date: 2018-12-12 15:11:11
  * @Last Modified by: KokoTa
- * @Last Modified time: 2018-12-12 17:57:07
+ * @Last Modified time: 2018-12-12 22:42:58
  */
 
- /**
-  * 粒子类
-  */
 class Ball {
-  constructor(x, y, r, speed, color) {
+  /**
+   * 粒子构造参数
+   * @param {Number} x 粒子的 X 轴坐标
+   * @param {Number} y 粒子的 Y 轴坐标
+   * @param {Number} r 粒子半径
+   * @param {Number} speedX 粒子 X 轴速度
+   * @param {Number} speedY 粒子 Y 轴速度
+   * @param {String} color 粒子颜色
+   * @param {Number} acX 粒子 X 轴加速度
+   * @param {Number} acY 粒子 Y 轴加速度
+   */
+  constructor(x, y, r, speedX, speedY, color, acX, acY) {
     this.x = x;
     this.y = y;
     this.radius = r;
-    this.speed = speed;
+    this.originSpeedX = speedX;
+    this.originSpeedY = speedY;
+    this.originAcX = acX;
+    this.originAcY = acY;
+    this.speedX = speedX;
+    this.speedY = speedY;
     this.color = color;
+    this.acX = acX;
+    this.acY = acY;
   }
-  // TODO 改为移动一段距离后变换方向
-  randomDistance(speed) {
-    const dis = (Math.random() * speed * 2) - speed;
-    if (dis === 0) return 1;
-    return dis;
+  // 获取 -originSpeed ~ originSpeed 范围内的某个值
+  randomSpeed(originSpeed) {
+    const speed = (Math.random() * originSpeed * 2) - originSpeed;
+    // 速度不能太小，否则粒子会抽搐
+    if (speed < 0.1 && speed > -0.1) return this.randomSpeed(originSpeed);
+    return speed;
   }
   // 绘制小球
   draw(context) {
     // 改变位置
-    const disX = this.randomDistance(this.speed);
-    const disY = this.randomDistance(this.speed);
-    this.x += disX;
-    this.y += disY;
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // 通过加速度来改变速度
+    this.speedX -= this.acX;
+    this.speedY -= this.acY;
+
+    // 当速度大于最大速度时，改变加速度方向
+    if (this.speedX > this.originSpeedX) {
+      this.acX = this.originAcX;
+    }
+    // 当速度小于最小速度时，改变加速度方向
+    else if (this.speedX < -this.originSpeedX) {
+      this.acX = -this.originAcX;
+    }
+    if (this.speedY > this.originSpeedY) {
+      this.acY = this.originAcY;
+    }
+    else if (this.speedY < -this.originSpeedY) {
+      this.acY = -this.originAcY;
+    }
+
+    // 当其中一个方向的速度接近 0 时，重新赋予速度，速度是随机的
+    if (Math.abs(this.speedX) - 0.01 < 0 || Math.abs(this.speedY) - 0.01 < 0) {
+      this.speedX = this.randomSpeed(this.originSpeedX);
+      this.speedY = this.randomSpeed(this.originSpeedY);
+    }
 
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
@@ -42,16 +81,18 @@ class Ball {
  * 生成器
  */
 class Generator {
-  constructor(id, number = 10, radiusRange = [4, 10],  speedRange = [0.5, 0.8], color = 'lightgray') {
+  constructor(id, number=500, radiusRange=[2, 5], speedRange=[-0.5, 0.5], color='lightgray', acX=0.01, acY=0.01) {
     this.canvas = document.querySelector(id);
     this.context = this.canvas.getContext('2d');
     this.balls = [];
-    this.number = number;
-    this.radiusRange = radiusRange;
-    this.speedRange = speedRange;
-    this.color = color;
-    this.clientWidth = this.canvas.clientWidth;
-    this.clientHeight = this.canvas.clientHeight;
+    this.number = number; // 粒子数量
+    this.radiusRange = radiusRange; // 半径范围
+    this.speedRange = speedRange; // 移动速度
+    this.color = color; // 粒子颜色
+    this.acX = acX; // X 方向加速度
+    this.acY = acY; // Y 方向加速度
+    this.clientWidth = this.canvas.clientWidth; // 画布宽度
+    this.clientHeight = this.canvas.clientHeight; // 画布高度
   }
   // 随机位置
   randomPosition() {
@@ -59,10 +100,11 @@ class Generator {
     const randomY = this.getFromRange(0, this.clientHeight);
     return { x: randomX, y: randomY };
   }
-  // 范围内随机取值
-  getFromRange(min = 0, max = 100) {
+  // 获取 min ~ max 内的某个值
+  getFromRange(min, max) {
     const value = (Math.random() * (max - min + 1)) + min;
-    if (value === 0) return this.getFromRange(min, max);
+    // 速度不能太小，否则粒子会抽搐
+    if (value < 0.1 && value > -0.1) return this.getFromRange(min, max);
     return value;
   }
   // 初始化
@@ -71,10 +113,13 @@ class Generator {
     // 获取小球列表
     for(let i = 0; i < number; i++) {
       const { x, y } = this.randomPosition();
-      const speed = this.getFromRange(...this.speedRange);
+      const speedX = this.getFromRange(...this.speedRange);
+      const speedY = this.getFromRange(...this.speedRange);
       const radius = this.getFromRange(...this.radiusRange);
+      const acX = this.acX;
+      const acY = this.acY;
       const color = this.color;
-      this.balls.push(new Ball(x, y, radius, speed, color));
+      this.balls.push(new Ball(x, y, radius, speedX, speedY, color, acX, acY));
     }
     // 开始绘制
     this.draw();
@@ -87,9 +132,6 @@ class Generator {
     // 绘制小球
     for(let i = 0; i < this.balls.length; i++) {
       const ball = this.balls[i];
-      if (i === 1) {
-        console.log(ball.speed);
-      }
       ball.draw(this.context);
     }
     window.requestAnimationFrame(this.draw.bind(this));
